f1afd577cdc04773eb00fee75fbf75818fcbd1ef
RonjaO <ronja.oja@gmail.com>
2016-11-25 22:26:47 +0200

    Jotain shittiä, jotta android kääntyy

--- app/src/main/java/com/bititpoikittain/mobiilimatkustaminen/MainActivity.java
+++ app/src/main/java/com/bititpoikittain/mobiilimatkustaminen/MainActivity.java
@@ -9,22 +9,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     
-    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); 
-        if (mBluetoothAdapter == null) {
-             // Device does not support Bluetooth 
-         } 
-         
-         if (!mBluetoothAdapter.isEnabled()) {
-             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-         }
          
     }
 }
