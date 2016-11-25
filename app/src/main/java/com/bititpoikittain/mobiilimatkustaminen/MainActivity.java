3d676d6c7d577cb98564d9375a7fca5891069ef0
RonjaO <ronja.oja@gmail.com>
2016-11-25 23:29:29 +0200

    Proximiion jotain koodia MainActivityssa.

--- app/src/main/java/com/bititpoikittain/mobiilimatkustaminen/MainActivity.java
+++ app/src/main/java/com/bititpoikittain/mobiilimatkustaminen/MainActivity.java
@@ -1,14 +1,22 @@
 package com.bititpoikittain.mobiilimatkustaminen;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
 
+import io.proximi.proximiiolibrary.Proximiio;
+import io.proximi.proximiiolibrary.ProximiioFactory;
+import io.proximi.proximiiolibrary.ProximiioFloor;
+import io.proximi.proximiiolibrary.ProximiioGeofence;
+import io.proximi.proximiiolibrary.ProximiioImageCallback;
+import io.proximi.proximiiolibrary.ProximiioListener;
+
+
 public class MainActivity extends AppCompatActivity {
-    
+
+    private Proximiio proximiio;
+    private ProximiioListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +24,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
          
     }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        proximiio = ProximiioFactory.getProximiio(this, this);
+        listener = new ProximiioListener() {
+            @Override
+            public void position(double lat, double lon, double accuracy) {
+                // Do something with the positioning system.
+                // See ProximiioListener in the docs for all available methods.
+            }
+        };
+        proximiio.addListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        proximiio.removeListener(listener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        proximiio.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        proximiio.onActivityResult(requestCode, resultCode, data);
+    }
+    
 }
