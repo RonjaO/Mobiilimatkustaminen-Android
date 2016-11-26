f446bdd7ac47c0fc6a57d416179db5afeb2f0cd3
Samuli Kärkkäinen <samuli.karkkainen@poplatek.fi>
2016-11-26 17:25:15 +0200

    Add activity "Active trip"

--- app/src/main/java/com/bititpoikittain/mobiilimatkustaminen/StartTripActivity.java
+++ app/src/main/java/com/bititpoikittain/mobiilimatkustaminen/StartTripActivity.java
@@ -1,7 +1,9 @@
 package com.bititpoikittain.mobiilimatkustaminen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class StartTripActivity extends AppCompatActivity {
     @Override
@@ -10,4 +12,8 @@ public class StartTripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start_trip);
     }
 
+    public void buyTrip(View view) {
+        Intent intent = new Intent(this, TripActiveActivity.class);
+        startActivity(intent);
+    }
 }
